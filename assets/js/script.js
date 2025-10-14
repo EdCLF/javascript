b74bec8e6b0775f730ec9f7935855fd8f80e8dbe
Edvaldo <dinho_canuto@yahoo.com.br>
2025-10-14 16:34:14 -0300

    segundo commit
    inicio dos testes

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var mensagem = 10;
+
+function mostrar_global(){
+    document.getElementById("saida").innerHTML = "valor Atual: "+ mensagem;
+}
+function alterar(){
+    mensagem = 50;
+    document.getElementById("saida").innerHTML = "Valor alterado:" + mensagem;
+    mensagem=10;
+}
+function test_let(){
+    let frase ="Olá, sou uma variavel LET";
+    document.getElementById("saida2").innerHTML = frase;
+}
+function limpa(){
+    let limpa = " ";
+    document.getElementById("saida2").innerHTML =limpa;
+}
+function muda(){
+    for(i=0;i<3;i++){
+        let paragrafos = document.getElementsByClassName("para");
+        paragrafos[i].style.color='var(--verde-s)';
+    }   
+}
